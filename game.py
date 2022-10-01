249f801197766e23ebcfa17cff135b118a5433d6
Alexandre Hayward <alexhayward4321@gmail.com>
2022-10-01 13:48:04 +0100

    Separating out a human playable game

---
+++ game.py
@@ -0,0 +1,186 @@
+# %%
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 26 15:39:58 2021
+
+@author: alexh
+"""
+
+import pickle
+import json
+import re
+import requests
+import random
+import string
+import secrets
+import time
+import pandas as pd
+import collections
+import numpy as np
+from datetime import datetime
+try:
+    from urllib.parse import parse_qs, urlencode, urlparse
+except ImportError:
+    from urlparse import parse_qs, urlparse
+    from urllib import urlencode
+
+# I added this bit
+
+from sklearn.model_selection import train_test_split
+
+
+class HangmanGameAPI(object):
+    def __init__(self):
+
+        # Attributes included in original base solution
+        self.guessed_letters = []
+        full_dictionary_location = "C:\\Users\\alexh\\OneDrive\\Documents\\Coding\\Python\\Personal Projects\\Hangman\\dictionaries\\words_250000_train.txt"
+        self.full_dictionary = self.build_dictionary(full_dictionary_location)
+        # ADDED ATTRIBUTES
+        # For my self-made start_game function to run
+        self.tries_remains = 7  # or 6
+        #######################
+
+    def human_guess(self):
+        letter = input("Please enter the letter you would like to guess: ")
+        while not letter.isalpha() or len(letter) != 1 or letter in self.guessed_letters:
+            if not letter.isalpha():
+                letter = input("Invalid input, please enter a single letter,\
+                    no special characters permitted: ")
+            elif len(letter) != 1:
+                letter = input("Invalid input, please enter a single letter: ")
+            else:
+                letter = input(
+                    "That letter has been guessed already, please try another one: ")
+        return letter
+
+    def build_dictionary(self, dictionary_file_location):
+        text_file = open(dictionary_file_location, "r")
+        full_dictionary = text_file.read().splitlines()
+        text_file.close()
+        return full_dictionary
+
+    def start_game(self, verbose=True, practice=1, AI=False, test=False):
+
+        # reset guessed letters to empty set and current plausible dictionary to the full dictionary
+        self.guessed_letters = []
+        self.tries_remains = 7
+
+        # Chooses a word from the validation dictionary and creates a hangman equivalent
+        # to show the user
+        if AI:
+            self.training_dictionary, self.validation_dictionary =\
+                train_test_split(self.full_dictionary, random_state=0)
+            word = random.choice(self.validation_dictionary)
+        else:
+            word = random.choice(self.full_dictionary)
+        hangman_word = "_ " * len(word)
+
+        if test:
+            print(f"This is the word you want to guess: {word}")
+
+        if verbose:
+            print("Successfully start a new game! # of tries remaining: {0}.\
+                  Word: {1}.".format(self.tries_remains, hangman_word))
+
+        if not AI:
+            while self.tries_remains > 0:
+                # get guessed letter from guess method
+
+                guess_letter = self.human_guess()
+
+                # append guessed letter to guessed letters field in hangman object
+                self.guessed_letters.append(guess_letter)
+
+                if verbose:
+                    print("Guessing letter: {0}".format(guess_letter))
+
+                # Check if your guessed letter is in the word, if not then decrement tries remaining
+                if guess_letter in word:
+                    indexes = [i for i, ltr in enumerate(
+                        word) if ltr == guess_letter]
+                    hangman_word_list = list(hangman_word)
+                    for idx in indexes:
+                        hangman_word_list[idx * 2] = guess_letter
+                    hangman_word = "".join(hangman_word_list)
+                    if verbose:
+                        print(f"Correct guess!   Updated word: {hangman_word}\
+                            # tries remaining: {self.tries_remains}")
+
+                else:
+                    self.tries_remains -= 1
+                    if verbose:
+                        print(f"Incorrect guess! Updated word: {hangman_word}\
+                            # tries remaining: {self.tries_remains}")
+
+                if self.tries_remains >= 0 and "_" not in hangman_word:
+                    if verbose:
+                        print("Successfully finished game!")
+                elif self.tries_remains == 0:
+                    if verbose:
+                        print('\n'.join(("Failed game. You ran out of lives.",
+                              f"The word you needed to guess: {word}")))
+        else:
+            while self.tries_remains > 0:
+                # get guessed letter from guess method
+                guess_letter = self.guess(hangman_word)
+
+                # append guessed letter to guessed letters field in hangman object
+                self.guessed_letters.append(guess_letter)
+
+                if verbose:
+                    print("Guessing letter: {0}".format(guess_letter))
+
+                # Check if your guessed letter is in the word, if not then decrement tries remaining
+                if guess_letter in word:
+                    indexes = [i for i, ltr in enumerate(
+                        word) if ltr == guess_letter]
+                    hangman_word_list = list(hangman_word)
+                    for idx in indexes:
+                        hangman_word_list[idx * 2] = guess_letter
+                    hangman_word = "".join(hangman_word_list)
+                    if verbose:
+                        print(f"Correct guess!   Updated word: {hangman_word}\
+                            # tries remaining: {self.tries_remains}")
+
+                else:
+                    self.tries_remains -= 1
+                    if verbose:
+                        print(f"Incorrect guess! Updated word: {hangman_word}\
+                            # tries remaining: {self.tries_remains}")
+
+                game_end = False
+                if self.tries_remains >= 0 and "_" not in hangman_word:
+                    if verbose:
+                        print("Successfully finished game!")
+
+                    game_end = True
+                elif self.tries_remains == 0:
+                    if verbose:
+                        print('\n'.join(("Failed game. You ran out of lives.",
+                              f"The word you needed to guess: {word}")))
+                    game_end = True
+                if game_end:
+                    # Update counts of appearances of regular expressions
+
+                    # Removing duplicates
+                    duplicates_removed = set(self.whole_game_regexes)
+                    for regex in duplicates_removed:
+                        if self.regex_df.loc[regex, "counter"] == 0:
+                            self.regex_df.loc[regex, "counter"] = 1
+                        else:
+                            self.regex_df.loc[regex,
+                                              "counter"] = self.regex_df.loc[regex, "counter"] + 1
+
+                    # Write DataFrame of all known pickles to external file for safekeeping
+                    self.regex_df.to_pickle(self.regex_df_path)
+
+        return None
+
+# %%
+
+
+game = HangmanGameAPI()
+game.start_game(test=True)
+
+# %%
